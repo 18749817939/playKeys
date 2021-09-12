af4ecb68267034bbccfe7aed0d207a0e507c0a58
18749817939 <mabojie@bupt.edu.cn>
2021-09-12 18:25:51 +0800

    test

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -17,7 +17,7 @@ export default class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <Text className='test'>Hello world!</Text>
       </View>
     )
   }
